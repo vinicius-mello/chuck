dcb18c91de28de2c892e6695ae85b5e78a1e8d2f
Vinicius Mello <vinicius.moreira.mello@gmail.com>
2025-08-27 19:34:50 -0300

    fast switch

--- chuck.c
+++ chuck.c
@@ -49,54 +49,55 @@ static int64_t ip;
 #define FLAG_IMMEDIATE (1LL << 62)
 #define CLEAR_FLAGS (~(FLAG_IMMEDIATE | FLAG_PRIMITIVE))
 
-#define PRIM_EXIT ( 0 | FLAG_PRIMITIVE ) 
-#define PRIM_ADD ( 1 | FLAG_PRIMITIVE )
-#define PRIM_SUB ( 2 | FLAG_PRIMITIVE )
-#define PRIM_MUL ( 3 | FLAG_PRIMITIVE )
-#define PRIM_SWAP ( 4 | FLAG_PRIMITIVE )
-#define PRIM_PICK ( 5 | FLAG_PRIMITIVE )
-#define PRIM_LIT ( 6 | FLAG_PRIMITIVE )
-#define PRIM_FIND ( 7 | FLAG_PRIMITIVE )
-#define PRIM_FETCH ( 8 | FLAG_PRIMITIVE )
-#define PRIM_CFETCH ( 9 | FLAG_PRIMITIVE )
-#define PRIM_STORE ( 10 | FLAG_PRIMITIVE )
-#define PRIM_QUIT ( 11 | FLAG_PRIMITIVE )
-#define PRIM_EXECUTE ( 12 | FLAG_PRIMITIVE )
-#define PRIM_HERE ( 13 | FLAG_PRIMITIVE )
-#define PRIM_TEXT ( 14 | FLAG_PRIMITIVE )
-#define PRIM_IMMEDIATE ( 15 | FLAG_PRIMITIVE )
-#define PRIM_COLON ( 16 | FLAG_PRIMITIVE | FLAG_IMMEDIATE )
-#define PRIM_DOT ( 17 | FLAG_PRIMITIVE )
-#define PRIM_REVEAL ( 18 | FLAG_PRIMITIVE | FLAG_IMMEDIATE )
-#define PRIM_SEMICOLON ( 19 | FLAG_PRIMITIVE | FLAG_IMMEDIATE )
-#define PRIM_CREATE ( 20 | FLAG_PRIMITIVE )
-#define PRIM_DOES ( 21 | FLAG_PRIMITIVE )
-#define PRIM_ALLOT ( 22 | FLAG_PRIMITIVE )
-#define PRIM_ALLOT_STR ( 23 | FLAG_PRIMITIVE )
-#define PRIM_COMP ( 24 | FLAG_PRIMITIVE )
-#define PRIM_COMPILE ( 25 | FLAG_PRIMITIVE )
-#define PRIM_POSTPONE ( 26 | FLAG_PRIMITIVE | FLAG_IMMEDIATE )
-#define PRIM_ZERO_BRANCH ( 27 | FLAG_PRIMITIVE )
-#define PRIM_BRANCH ( 28 | FLAG_PRIMITIVE )
-#define PRIM_FROM_RETURN ( 29 | FLAG_PRIMITIVE )
-#define PRIM_TO_RETURN ( 30 | FLAG_PRIMITIVE )
-#define PRIM_MARK ( 31 | FLAG_PRIMITIVE )
-#define PRIM_RESOLVE ( 32 | FLAG_PRIMITIVE )
-#define PRIM_EMIT ( 33 | FLAG_PRIMITIVE )
-#define PRIM_TYPE ( 34 | FLAG_PRIMITIVE )
-#define PRIM_COMMA ( 35 | FLAG_PRIMITIVE )
-#define PRIM_EQUAL ( 36 | FLAG_PRIMITIVE )
-#define PRIM_LESSER_THAN ( 37 | FLAG_PRIMITIVE )
-#define PRIM_GREATER_THAN ( 38 | FLAG_PRIMITIVE )
-#define PRIM_PARSE ( 39 | FLAG_PRIMITIVE )
-#define PRIM_DUP ( 40 | FLAG_PRIMITIVE )
-#define PRIM_DROP ( 41 | FLAG_PRIMITIVE )
-#define PRIM_PACK ( 42 | FLAG_PRIMITIVE )
-#define PRIM_UNPACK ( 43 | FLAG_PRIMITIVE )
-#define PRIM_ENTRY ( 44 | FLAG_PRIMITIVE )
-#define PRIM_DIV ( 45 | FLAG_PRIMITIVE )
-#define PRIM_MOD ( 46 | FLAG_PRIMITIVE )
-#define PRIM_TOP_RETURN ( 47 | FLAG_PRIMITIVE )
+#define _EXIT          0 
+#define _ADD           1 
+#define _SUB           2
+#define _MUL           3
+#define _SWAP  			   4 
+#define _PICK  	       5 
+#define _LIT           6 
+#define _FIND          7 
+#define _FETCH         8 
+#define _CFETCH        9 
+#define _STORE        10 
+#define _QUIT         11 
+#define _EXECUTE      12 
+#define _HERE         13 
+#define _TEXT         14 
+#define _IMMEDIATE    15 
+#define _COLON        16 
+#define _DOT          17 
+#define _REVEAL       18 
+#define _SEMICOLON    19 
+#define _CREATE       20 
+#define _DOES         21 
+#define _ALLOT        22 
+#define _ALLOT_STR    23 
+#define _COMP         24 
+#define _COMPILE      25 
+#define _POSTPONE     26 
+#define _ZERO_BRANCH  27 
+#define _BRANCH       28 
+#define _FROM_RETURN  29 
+#define _TO_RETURN    30 
+#define _MARK         31 
+#define _RESOLVE      32 
+#define _EMIT         33 
+#define _TYPE         34 
+#define _COMMA        35 
+#define _EQUAL        36 
+#define _LESSER_THAN  37 
+#define _GREATER_THAN 38 
+#define _PARSE        39 
+#define _DUP          40 
+#define _DROP         41 
+#define _PACK         42 
+#define _UNPACK       43 
+#define _ENTRY        44 
+#define _DIV          45 
+#define _MOD          46 
+#define _TOP_RETURN   47 
+#define _CSTORE       48 
 
 typedef struct dict_entry {
   char * symbol;
@@ -106,54 +107,55 @@ typedef struct dict_entry {
 static int64_t latest;
 
 static dict_entry dictionary[] = {
-  {"exit", PRIM_EXIT},
-  {"(lit)", PRIM_LIT},  
-  {"find", PRIM_FIND},  
-  {"+", PRIM_ADD},
-  {"-", PRIM_SUB},
-  {"@", PRIM_FETCH},
-  {"c@", PRIM_CFETCH},
-  {"!", PRIM_STORE},
-  {":", PRIM_COLON},
-  {";", PRIM_SEMICOLON},
-  {".", PRIM_DOT},
-  {"reveal", PRIM_REVEAL},   
-  {"quit", PRIM_QUIT},  
-  {"postpone", PRIM_POSTPONE},
-  {"(comp)", PRIM_COMP},
-  {"compile", PRIM_COMPILE},
-  {"exec", PRIM_EXECUTE},
-  {"here", PRIM_HERE},
-  {"text", PRIM_TEXT},
-  {"immediate", PRIM_IMMEDIATE},
-  {"0branch", PRIM_ZERO_BRANCH},
-  {"r>", PRIM_FROM_RETURN},
-  {">r", PRIM_TO_RETURN},
-  {"r@", PRIM_TOP_RETURN},
-  {">mark", PRIM_MARK},
-  {"<resolve", PRIM_RESOLVE},
-  {"emit", PRIM_EMIT},
-  {"type", PRIM_TYPE},
-  {"branch", PRIM_BRANCH}, 
-  {"swap", PRIM_SWAP},
-  {"pick", PRIM_PICK},
-  {"create", PRIM_CREATE},
-  {",", PRIM_COMMA},
-  {"allot", PRIM_ALLOT},
-  {"allot\"", PRIM_ALLOT_STR},
-  {"(does>)", PRIM_DOES},
-  {"=", PRIM_EQUAL},
-  {"<", PRIM_LESSER_THAN},
-  {">", PRIM_GREATER_THAN},
-  {"parse", PRIM_PARSE},
-  {"dup", PRIM_DUP},
-  {"drop", PRIM_DROP},
-  {"*", PRIM_MUL},
-  {"pack", PRIM_PACK},
-  {"unpack", PRIM_UNPACK},
-  {"entry", PRIM_ENTRY},
-  {"/", PRIM_DIV},
-  {"mod", PRIM_MOD},
+  {"exit", _EXIT},
+  {"(lit)", _LIT},  
+  {"find", _FIND},  
+  {"+", _ADD},
+  {"-", _SUB},
+  {"@", _FETCH},
+  {"c@", _CFETCH},
+  {"!", _STORE},
+  {":", _COLON | FLAG_IMMEDIATE},
+  {";", _SEMICOLON | FLAG_IMMEDIATE},
+  {".", _DOT},
+  {"reveal", _REVEAL | FLAG_IMMEDIATE},   
+  {"quit", _QUIT},  
+  {"postpone", _POSTPONE | FLAG_IMMEDIATE},
+  {"(comp)", _COMP},
+  {"compile", _COMPILE},
+  {"exec", _EXECUTE},
+  {"here", _HERE},
+  {"text", _TEXT},
+  {"immediate", _IMMEDIATE},
+  {"0branch", _ZERO_BRANCH},
+  {"r>", _FROM_RETURN},
+  {">r", _TO_RETURN},
+  {"r@", _TOP_RETURN},
+  {">mark", _MARK},
+  {"<resolve", _RESOLVE},
+  {"emit", _EMIT},
+  {"type", _TYPE},
+  {"branch", _BRANCH}, 
+  {"swap", _SWAP},
+  {"pick", _PICK},
+  {"create", _CREATE},
+  {",", _COMMA},
+  {"allot", _ALLOT},
+  {"allot\"", _ALLOT_STR},
+  {"(does>)", _DOES},
+  {"=", _EQUAL},
+  {"<", _LESSER_THAN},
+  {">", _GREATER_THAN},
+  {"parse", _PARSE},
+  {"dup", _DUP},
+  {"drop", _DROP},
+  {"*", _MUL},
+  {"pack", _PACK},
+  {"unpack", _UNPACK},
+  {"entry", _ENTRY},
+  {"/", _DIV},
+  {"mod", _MOD},
+  {"c!", _CSTORE},
 };
 
 #define MAX_DATA 4096
@@ -196,7 +198,7 @@ void init() {
 		pair_int pi = { .a = text, .b = strlen(e.symbol) };
 	  strncpy(&strings[pi.a], e.symbol, pi.b);
 		text += pi.b; 
-		int64_t tok = e.address;
+		int64_t tok = e.address | FLAG_PRIMITIVE;
 		data[here++] = (cell) tok;
 		data[here++] = (cell) pi;
 		data[here++] = (cell) prev;
@@ -245,7 +247,7 @@ void parse() {
 		l++;in++;
 	}
 	in += 1;
-	strings[text+l] = 0;
+	strings[text+l] = 0; // strtoll
 	while(in<ntib && isspace(strings[in])) in++;
 	pair_int pi = { .a=text, .b=l };
   PUSH(dataStack, pi);
@@ -276,207 +278,213 @@ void reveal() {
 }
 
 void exec(int64_t t) {
+	t &= CLEAR_FLAGS;
 	int64_t a, b; 
   cell ca, cb;
 	pair_int pi;
 	switch(t) {
-		case PRIM_EXIT:
+		case _EXIT:
 	  	ip = POP(returnStack).i;
 			break;
-		case PRIM_ADD:
+		case _ADD:
   		a = POP(dataStack).i;
   		b = POP(dataStack).i;
   		PUSH(dataStack, b+a);
 			break;
-		case PRIM_SUB: 
+		case _SUB: 
   		a = POP(dataStack).i;
   		b = POP(dataStack).i;
   		PUSH(dataStack, b-a);
 			break;
-		case PRIM_MUL:
+		case _MUL:
 			a = POP(dataStack).i;
 			b = POP(dataStack).i;
 			PUSH(dataStack, b*a);
 			break;    
-		case PRIM_SWAP:
+		case _SWAP:
 		  ca = POP(dataStack);
   		cb = POP(dataStack);
   		PUSH(dataStack, ca);
   		PUSH(dataStack, cb);
 			break;
-		case PRIM_PICK:
+		case _PICK:
   		a = POP(dataStack).i;
   		PUSH(dataStack, dataStack[dataStackTop-a]);
 			break;
-		case PRIM_LIT:
+		case _LIT:
   		PUSH(dataStack, program[++ip].i);
 			break;
-		case PRIM_FETCH:
+		case _FIND: 
+			find();
+			break;
+		case _FETCH:
   		a = POP(dataStack).i;
   		PUSH(dataStack, data[a]);
 			break;
-		case PRIM_CFETCH:
+		case _CFETCH:
 		  a = POP(dataStack).i;
   		PUSH(dataStack, (int64_t)strings[a]);
 			break;
-		case PRIM_STORE:
+		case _STORE:
   		a = POP(dataStack).i;
   		cb = POP(dataStack);
   		data[a] = cb;
 			break;
-		case PRIM_QUIT:
+		case _QUIT:
 		  exit(0);
 			break;
-		case PRIM_HERE:
-		  PUSH(dataStack, here);
-			break;
-		case PRIM_TEXT:
-		  PUSH(dataStack, text);
-			break;
-		case PRIM_IMMEDIATE:
-		  data[latest].i |= FLAG_IMMEDIATE;
-			break;
-		case PRIM_FIND: 
-			find();
-			break;
-		case PRIM_EXECUTE:
+		case _EXECUTE:
       PUSH(returnStack, ip);
 			execute();
 			ip = POP(returnStack).i;
 			break;
-		case PRIM_ENTRY: 
-		  entry(); 
+		case _HERE:
+		  PUSH(dataStack, here);
 			break;
-		case PRIM_COLON:
+		case _TEXT:
+		  PUSH(dataStack, text);
+			break;
+		case _IMMEDIATE:
+		  data[latest].i |= FLAG_IMMEDIATE;
+			break;
+		case _COLON:
 		  (*state).i = 1;
 			entry();
 			break;
-		case PRIM_DOT:
+		case _DOT:
 		  printf("%lld ", POP(dataStack));
 			break;
-		case PRIM_REVEAL:
+		case _REVEAL:
 			reveal();
 			break;
-		case PRIM_SEMICOLON:
-		  compile(PRIM_EXIT);
+		case _SEMICOLON:
+		  compile(_EXIT | FLAG_PRIMITIVE);
 			(*state).i = 0;  
   		reveal();
 			break;
-		case PRIM_CREATE:
+		case _CREATE:
 		  entry();
-			compile(PRIM_LIT);
+			compile(_LIT | FLAG_PRIMITIVE);
   		compile(here);
-  		compile(PRIM_EXIT);
-  		compile(PRIM_EXIT);
+  		compile(_EXIT | FLAG_PRIMITIVE);
+  		compile(_EXIT | FLAG_PRIMITIVE);
   		reveal();
 			break;
-		case PRIM_DOES:
+		case _DOES:
   		a = data[latest].i & CLEAR_FLAGS;
-  		program[a+2].i = PRIM_BRANCH;
+  		program[a+2].i = _BRANCH | FLAG_PRIMITIVE;
   		program[a+3].i = ip + 2;
 			break;
-		case PRIM_COMP:
+		case _ALLOT:
+  		a = POP(dataStack).i;
+  		here += a;
+			break;
+		case _ALLOT_STR:
+			a = POP(dataStack).i;
+			text += a;
+			break;
+		case _COMP:
 		  compile(program[++ip].i);
 			break;
-		case PRIM_COMPILE:
+		case _COMPILE:
 		  compile(POP(dataStack).i);
 			break;
-		case PRIM_POSTPONE:
+		case _POSTPONE:
 			PUSH(dataStack, (int64_t)32);
 			parse();
 		  find();
-			compile(PRIM_COMP);
+			compile(_COMP | FLAG_PRIMITIVE);
   		compile(data[POP(dataStack).i].i);
 			break;
-		case PRIM_ZERO_BRANCH:
+		case _ZERO_BRANCH:
 		  if(POP(dataStack).i == 0)
     		ip = program[ip+1].i-1;
   		else ++ip;
 			break;
-		case PRIM_BRANCH:
+		case _BRANCH:
 		  ip = program[ip+1].i-1;
 			break;
-		case PRIM_FROM_RETURN:
+		case _FROM_RETURN:
 		  PUSH(dataStack, POP(returnStack));
 			break;
-		case PRIM_TO_RETURN:
+		case _TO_RETURN:
   		PUSH(returnStack, POP(dataStack));
 			break;
-		case PRIM_TOP_RETURN:
-  		PUSH(dataStack, TOP(returnStack));
-			break;
-		case PRIM_MARK:
+		case _MARK:
 		  PUSH(dataStack, code);
 			break;
-		case PRIM_RESOLVE:
+		case _RESOLVE:
 			a = POP(dataStack).i;
   		program[a].i = code;
 			break;
-		case PRIM_EMIT:
+		case _EMIT:
 			a = POP(dataStack).i;
   		putchar((char) a);
 			break;
-		case PRIM_TYPE:
+		case _TYPE:
   		pi = POP(dataStack).pi;
   		str_print(pi, 0);
 			break;
-		case PRIM_COMMA:
+		case _COMMA:
   		ca = POP(dataStack);
   		data[here++] = ca;
 			break;
-		case PRIM_ALLOT:
-  		a = POP(dataStack).i;
-  		here += a;
-			break;
-		case PRIM_ALLOT_STR:
-			a = POP(dataStack).i;
-			text += a;
-			break;
-		case PRIM_EQUAL:
+		case _EQUAL:
   		a = POP(dataStack).i;
   		b = POP(dataStack).i;
   		PUSH(dataStack, (int64_t)(b == a));
 			break;
-		case PRIM_LESSER_THAN:
+		case _LESSER_THAN:
 		  a = POP(dataStack).i;
 		  b = POP(dataStack).i;
 		  PUSH(dataStack, (int64_t)(b < a));
 			break;
-		case PRIM_GREATER_THAN:
+		case _GREATER_THAN:
 		  a = POP(dataStack).i;
   		b = POP(dataStack).i;
   		PUSH(dataStack, (int64_t)(b > a));
 			break;
-		case PRIM_PARSE: 
+		case _PARSE: 
 			parse();
 			break;
-		case PRIM_DUP:
+		case _DUP:
 		  PUSH(dataStack, TOP(dataStack));
 			break;
-		case PRIM_DROP:
+		case _DROP:
 		  DROP(dataStack);
 			break;
-		case PRIM_PACK:
+		case _PACK:
   		b = POP(dataStack).i;
   		a = POP(dataStack).i;
   		pi.a = (int32_t)a;
 			pi.b = (int32_t)b;
   		PUSH(dataStack, pi);
 			break;
-		case PRIM_UNPACK:
+		case _UNPACK:
 		  pi = POP(dataStack).pi;
   		PUSH(dataStack, (int64_t) pi.a);
   		PUSH(dataStack, (int64_t) pi.b);
 			break;
-		case PRIM_DIV:
+		case _ENTRY: 
+		  entry(); 
+			break;
+		case _DIV:
 		  a = POP(dataStack).i;
 		  b = POP(dataStack).i;
 		  PUSH(dataStack, (b / a));
 			break;
-		case PRIM_MOD:
+		case _MOD:
 		  a = POP(dataStack).i;
 		  b = POP(dataStack).i;
 		  PUSH(dataStack, (b % a));
+			break;
+		case _TOP_RETURN:
+  		PUSH(dataStack, TOP(returnStack));
+			break;
+		case _CSTORE:
+		  a = POP(dataStack).i;
+  		cb = POP(dataStack);
+  		strings[a] = (char) cb.i;
 			break;
 	}
 }
@@ -500,9 +508,9 @@ void interpreter() {
     } else {
 			const char * b = &strings[text]; 
 			char * e; 
-			int64_t n = strtol(b, &e, 10);
+			int64_t n = strtoll(b, &e, 10);
       if((*state).i) {
-        compile(PRIM_LIT);
+        compile(_LIT | FLAG_PRIMITIVE);
         compile(n);
       } else
         PUSH(dataStack, n);
